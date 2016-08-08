217f3f96f505dc408f4332aa32916f03f972a221
Jongsoo Park <jongsoo.park@intel.com>
2016-08-08 11:42:18 -0700

    align with page boundary

--- src/base.c
+++ src/base.c
@@ -6,7 +6,7 @@ void * splatt_malloc(
     size_t const bytes)
 {
   void * ptr;
-  posix_memalign(&ptr, 64, bytes);
+  posix_memalign(&ptr, 4096, bytes);
   if(ptr == NULL) {
     fprintf(stderr, "SPLATT: posix_memalign() returned NULL.\n");
   }
