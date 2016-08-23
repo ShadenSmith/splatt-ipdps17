7daba3d9eb43ab97c86b0d5068f99d9eeb700108
Jongsoo Park <jongsoo.park@intel.com>
2016-08-23 00:51:08 -0700

    fix simd macro errors for avx2

--- src/simd_utils.h
+++ src/simd_utils.h
@@ -23,10 +23,11 @@
 #define SPLATT_VLEN (4)
 #define SPLATT_SIMDFPTYPE __m256d
 
-#define _MM_SET1(a) _mm256_set1_pd(b)
+#define _MM_SET1(a) _mm256_set1_pd(a)
 #define _MM_SETZERO() _mm256_setzero_pd()
 
 #define _MM_ADD(a, b) _mm256_add_pd(a, b)
+#define _MM_MUL(a, b) _mm256_mul_pd(a, b)
 #define _MM_FMADD(a, b, c) _mm256_fmadd_pd(a, b, c)
 
 #define _MM_LOAD(a) _mm256_load_pd(a)
